446ef1b61dc01ef2a24430b98267741a4bea8942
LuchoCreativo2 <luisd.guglietta@gmail.com>
2025-08-18 17:29:14 -0400

    Nombramiento de variables

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-console.log("Probando visualización desde lejos");
+//Funcion de lista 
+//El array es indexado a un número empezando desde el 0 
+//Yo necesito que cuando le des al botón se guarde el nombre en la lista 
+
+let amigos = ["Pepe","Andres"];
+let btn = document.getElementById("amigo");
+let listaAmigoSecreto = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
+function agregarAmigo (){
+
+}
+agregarAmigo();
