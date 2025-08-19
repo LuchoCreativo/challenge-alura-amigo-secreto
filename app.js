a7d69d9b42781d6667615bb7bcd642cbb6f55359
LuchoCreativo2 <luisd.guglietta@gmail.com>
2025-08-18 23:33:49 -0400

    fucionan los botones

--- app.js
+++ app.js
@@ -3,11 +3,12 @@
 //El array es indexado a un número empezando desde el 0 
 //Yo necesito que cuando le des al botón se guarde el nombre en la lista 
 
-let amigos = ["Pepe","Andres"];
-let btn = document.getElementById("amigo");
-let listaAmigoSecreto = document.getElementById("listaAmigos");
-let resultado = document.getElementById("resultado");
-function agregarAmigo (){
-
+let amigos = [];
+function agregarAmigo() {
+  alert("¡Botón de agregar amigos funciona!");
 }
-agregarAmigo();
+
+
+function sortearAmigo() {
+  alert("¡Botón de sortear amigo funciona!");
+}
