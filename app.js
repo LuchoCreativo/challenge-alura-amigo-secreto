98bfed61965051fa4f96f1478511103e427a813d
LuchoCreativo2 <luisd.guglietta@gmail.com>
2025-08-19 21:37:48 -0400

    Agregar funciones

--- app.js
+++ app.js
@@ -9,10 +9,45 @@ let amigos = [];
 
 function agregarAmigo() {
  let nombre = document.getElementById("amigo").value;
- console.log(nombre);
-}
+if(nombre){
+  amigos.push(nombre);
+  console.log(amigos);
+  console.log(nombre);
+}  else {
+    alert("Por favor ingresa un nombre válido");
+  }
 
+mostrarlistaAmigos();
+limpiarCampos();
+}
+/*
+function mostrarAlertaVacio() {
+if (nombre != ""){
+ alert("Ingresa un nombre correctamente");
+}
+}
+*/
+
+function mostrarlistaAmigos(agregarAmigo){
+    let lista = document.getElementById("listaAmigos");
+
+amigos.forEach(function (elemento, indice, array) {
+  console.log(elemento, indice);
+});
+// Manzana 0
+// Banana 1
+
+
+}
 
 function sortearAmigo() {
   alert("¡Botón de sortear amigo funciona!");
+
+
+}
+
+
+function limpiarCampos(){
+    document.getElementById("amigo").value = "";
+
 }
