3be04c01af51f09b597c950682fb96aa554bfcd0
LuchoCreativo2 <luisd.guglietta@gmail.com>
2025-08-26 17:56:39 -0400

    Limpieza de comentarios

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//Funcion de lista 
-//El array es indexado a un número empezando desde el 0 
-//Yo necesito que cuando le des al botón se guarde el nombre en la lista 
-
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 let amigos = [];
-
-
 
 function agregarAmigo() {
  let nombre = document.getElementById("amigo").value;
@@ -28,19 +22,11 @@ function mostrarListaAmigos() {
   lista.innerHTML = "";
 
 for (let i = 0; i < amigos.length; i++) {
-    let li = document.createElement("li"); // 👉 aquí debe ser "li" y no "listaAmigos"
+    let li = document.createElement("li"); // aquí se crea el  "li" 
     li.textContent = amigos[i];            // el nombre en la posición i
     lista.appendChild(li);                 // agregamos <li> al <ul>
   
 
-  /* Recorrer el array amigos con forEach
-  amigos.forEach(function (elemento, indice) {
-    console.log(elemento, indice);
-
-    //let li = document.createElement("li"); // Crear un <li> nuevo
-    li.textContent = elemento;             // Poner el nombre dentro del <li>
-    lista.appendChild(li);                 // Agregar el <li> al <ul>
-    */
   }
 }
 
